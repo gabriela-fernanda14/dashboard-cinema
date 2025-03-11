30d655a579fde601883362da754f47751f4dbc7c
Gabriela Fernanda <gabriela.f.barbosa@aluno.senai.br>
2025-03-11 11:03:33 -0300

    feat: create dashboardContent component

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,17 +1,23 @@
-
-import Header from "./components/header";
+import Header from "../components/header";
 import styles from "./page.module.css";
-import Sidebar from "./components/sidebar";
+import Sidebar from "../components/sidebar";
+import DashboardContent from "../components/dashboardContent";
 
 export default function Home() {
   return (
     <div>
       <Header />
 
-      <main className= {styles.dashboardContainer}>
+      <main className={styles.dashboardContainer}>
       <Sidebar />
+
+      <DashboardContent/>
+      
       </main>
 
     </div>
   );
 }
+
+
+    
--- src/components/sidebar/index.jsx
+++ src/components/sidebar/index.jsx
@@ -2,13 +2,13 @@ import styles from './sidebar.module.css';
 
 const Sidebar = () => {
     return (
-        <section class={styles.sidebar}>
-        <div class={styles.sidebarHeader}>
+        <section className={styles.sidebar}>
+        <div className={styles.sidebarHeader}>
           <h2>Menu</h2>
         </div>
-        <div class={styles.sidebarMenu}>
+        <div className={styles.sidebarMenu}>
           <ul>
-            <li><a href="#" class={styles.active}>Visão Geral</a></li>
+            <li><a href="#" className={styles.active}>Visão Geral</a></li>
             <li><a href="#">Vendas</a></li>
             <li><a href="#">Ocupação</a></li>
             <li><a href="#">Bomboniere</a></li>
